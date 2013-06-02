42159cb710b7813102e82486c44093e382e974c9
David Greenberg <dsg123456789@gmail.com>
2013-06-02 18:07:46 -0400

    Add sample usage of a scheduler

--- src/clj_mesos/scheduler.clj
+++ src/clj_mesos/scheduler.clj
@@ -13,3 +13,12 @@
                  (into {}))]
     (list* 'reify 'org.apache.mesos.Scheduler (make-reify-body org.apache.mesos.Scheduler fns))))
 
+(comment
+  (def myscheduler (scheduler (registered [driver fid mi]
+                                          (println "registered" fid mi)) 
+                              (resourceOffers [driver offers] (clojure.pprint/pprint offers))))
+  (def fi (map->proto org.apache.mesos.Protos$FrameworkInfo {:user "" :name "testframework"}))
+  (def overdriver
+    (org.apache.mesos.MesosSchedulerDriver.
+      myscheduler fi "localhost:5050"))
+  (start overdriver))
