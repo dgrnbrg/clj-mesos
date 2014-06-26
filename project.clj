f43b7662c2fdea907e41bb130d472cba81f5019a
Niklas Q. Nielsen <niklas@mesosphere.io>
2014-06-25 20:38:56 -0700

    Readd -SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-mesos "0.1.0"
+(defproject clj-mesos "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
