d048da71955b58379bbd93cdf255dc73e050ccd6
David Greenberg <dsg123456789@gmail.com>
2014-08-22 10:34:51 -0500

    Bump to mesos 0.20.0

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject clj-mesos "0.1.0-SNAPSHOT"
+(defproject clj-mesos "0.20.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.google.protobuf/protobuf-java "2.5.0"]
-                 [org.apache.mesos/mesos "0.19.0"]]
+                 [org.apache.mesos/mesos "0.20.0"]]
   :global-vars {*warn-on-reflection* true}
   :jvm-opts ["-XX:-OmitStackTraceInFastThrow" "-Xcheck:jni"]
   :repositories {"apache-releases" "http://repository.apache.org/content/repositories/releases/"})
