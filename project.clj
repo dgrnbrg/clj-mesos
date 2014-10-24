f40926420bf5e3e9e7fb94a2aa1156b5c790f89b
David Greenberg <dsg123456789@gmail.com>
2014-10-24 15:31:28 -0400

    Prep for 0.20.5 release

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clj-mesos "0.20.4"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject clj-mesos "0.20.5"
+  :description "A fully-featured Mesos binding for Clojure"
+  :url "http://github.com/dgrnbrg/clj-mesos"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
@@ -8,4 +8,5 @@
                  [org.apache.mesos/mesos "0.20.0"]]
   :global-vars {*warn-on-reflection* true}
   :jvm-opts ["-XX:-OmitStackTraceInFastThrow" "-Xcheck:jni"]
+  :deploy-repositories  [["releases" :clojars]]
   :repositories {"apache-releases" "http://repository.apache.org/content/repositories/releases/"})
