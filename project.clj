227369c553e570a729e04b258be5d3aca0eb7071
David Greenberg <dsg123456789@gmail.com>
2015-09-04 14:18:04 -0400

    Move to SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-mesos "0.22.0"
+(defproject clj-mesos "0.22.1-SNAPSHOT"
   :description "A fully-featured Mesos binding for Clojure"
   :url "http://github.com/dgrnbrg/clj-mesos"
   :license {:name "Eclipse Public License"
