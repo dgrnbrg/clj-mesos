8611516546f850609e9cf2611b0ad2c7b27f343b
David Greenberg <dsg123456789@gmail.com>
2015-09-16 14:45:17 -0400

    Bump version for deployment

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-mesos "0.22.1-SNAPSHOT"
+(defproject clj-mesos "0.22.1"
   :description "A fully-featured Mesos binding for Clojure"
   :url "http://github.com/dgrnbrg/clj-mesos"
   :license {:name "Eclipse Public License"
